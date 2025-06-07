0d9cffdb09afcc16377ea6598c722f40b623c814
Luiz Antonio <louis_work80@outlook.com>
2025-06-07 13:22:37 -0300

    Create the pin service

---
+++ services/pin-service/src/test/java/com/luiz/pin_service/PinServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.luiz.pin_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PinServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
