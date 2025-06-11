00e1d1861718e12fab8859622f002b625f2ec29f
Luiz Antonio <louis_work80@outlook.com>
2025-06-11 16:05:57 -0300

    Create pin-board-service

---
+++ services/pin-board-service/src/main/java/com/luiz/pin_board_service/PinBoardServiceApplication.java
@@ -0,0 +1,13 @@
+package com.luiz.pin_board_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PinBoardServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PinBoardServiceApplication.class, args);
+	}
+
+}
